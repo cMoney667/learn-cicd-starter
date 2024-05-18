e999c0d2395f6e41c6795b1c0800134fddca377a
cMoney667 <ococncol@gmail.com>
2024-05-18 14:38:55 -0400

    testing out format

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func main() {
+func main(){
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
@@ -52,6 +52,7 @@ func main() {
 		apiCfg.DB = dbQueries
 		log.Println("Connected to database!")
 	}
+
 
 	router := chi.NewRouter()
 
